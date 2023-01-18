138895176b324d666ca304ba3cadc51a947fb18e
Atharva Joshi <atharvajoshi@C02FN474Q6LR.local>
2023-01-18 19:04:20 +0530

    netlify changes

--- src/Urls.js
+++ src/Urls.js
@@ -1,4 +1,18 @@
-const PREFIX = "http://127.0.0.1:8000";
+// let PREFIX = "";
+
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//   // dev code
+//   PREFIX = "http://127.0.0.1:8000";
+// } else {
+//   // production code
+//   PREFIX = "https://a3harva.pythonanywhere.com";
+// }
+
+let PREFIX =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:8000"
+    : "https://a3harva.pythonanywhere.com";
+
 const URLS = {
   GET_COMMANDS: PREFIX + "/api/command",
 };
